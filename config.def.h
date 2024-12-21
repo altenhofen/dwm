320c3dfa3e9f46e0c17577d310f691f0568c151a
altenhofen <augustoaltenhofen@protonmail.com>
2024-12-21 15:11:36 -0300

    remove inc master

--- config.def.h
+++ config.def.h
@@ -74,7 +74,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	// { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	// { MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_r,      quit,           {0} },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
